c06a1298ea1ac7a0f2a54e716468462d204657a5
Ahmed Elzubair <ahmed.elzubair@lendo.sa>
2023-09-14 19:42:10 +0300

    init commit

---
+++ src/main/java/sa/com/tree/account/statment/treecodingchallenge/TreeCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package sa.com.tree.account.statment.treecodingchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeCodingChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreeCodingChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sa/com/tree/account/statment/treecodingchallenge/TreeCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package sa.com.tree.account.statment.treecodingchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeCodingChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
